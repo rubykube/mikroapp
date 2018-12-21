761c19ca5c15acec5f288562e93d202261d5c4d0
antonburduzha <anton.burduzha@hotmail.com>
2018-12-21 12:09:04 +0200

    minor fixes for .env and extra files;

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-require('dotenv').config();
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
