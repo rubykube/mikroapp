7b7fd316c4329e60f5a070550f30e6c9101a5feb
Alexander Buzin <alexbuzin88@gmail.com>
2018-12-29 12:23:00 +0200

    Fix CI & App component

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,51 +1,18 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import compose from 'recompose/compose';
-import { Route, Redirect, Switch } from 'react-router-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { withStyles } from '@material-ui/core/styles';
-import NavBar from '../../components/common/NavBar/index';
-import Typography from '@material-ui/core/Typography';
-import WalletPage from '../WalletPage';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import './styles.css';
-import { styles, muiTheme } from './styles';
-import actions from '../../actions';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
 
+import { store } from '../../store';
+import { history } from '../../history';
 
-class App extends Component {
-  componentDidMount() {
-    this.props.actions.fetchAccount();
-  }
+import AppInterface from './AppInterface';
 
-  renderAppMessage = text => <Typography variant="h4" style={{padding: 40}}>{text}</Typography>
+const App = () => (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <AppInterface />
+    </ConnectedRouter>
+  </Provider>
+);
 
-  render() {
-    const { classes, account } = this.props;
-
-    return (
-      <MuiThemeProvider theme={muiTheme}>
-        <div>
-          <NavBar user={account} />
-          <div className={classes.toolbar} />
-          <div className={classes.root}>
-            <CssBaseline />
-            <Switch>
-              <Redirect exact from='/' to='/wallets' />
-              <Route path="/trade" exact render={() => this.renderAppMessage('Trades coming soon!')}/>
-              <Route path="/wallets" component={WalletPage}/>
-            </Switch>
-          </div>
-        </div>
-      </MuiThemeProvider>
-    );
-  }
-}
-
-export default compose(
-  withStyles(styles),
-  connect(state => ({
-    isFetching: state.account.isFetching,
-    error: state.account.error
-  }), actions)
-)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'connected-react-router';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
-import { store } from './store';
-import { history } from './history';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <App />
-    </ConnectedRouter>
-  </Provider>,
+  <App />,
   document.getElementById('root')
 );
 
