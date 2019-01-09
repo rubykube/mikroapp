66e1397e657df33751fc84fe9f60bc51bc5bb113
Alexander Buzin <alexbuzin88@gmail.com>
2019-01-09 20:56:45 +0200

    Remove api/account

--- src/api/account.js
+++
@@ -1,7 +0,0 @@
-import axiosInstance from './requestBuilder';
-
-
-export const getAccount = () => {
-  return axiosInstance.get('/api/v2/barong/resource/users/me')
-    .then(response => response.data);
-};
