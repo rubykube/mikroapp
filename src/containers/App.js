89a631264aa3594a05a90ab287c4cc00336f6e0c
Alexander <oyershov@heliostech.fr>
2019-01-11 11:04:48 +0200

    Fix login user (#15)
    * Fix login user
    
    * Fix login - code rewriting

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,7 +8,6 @@ import LoginPage from './LoginPage';
 import actions from "../actions";
 import PrivateRoute from '../components/PrivateRoute';
 
-
 const muiTheme = createMuiTheme({
   palette: {
     primary: {
@@ -31,7 +30,11 @@ class App extends Component {
 
   render() {
     const { isFetching, user } = this.props;
-    const isAuthenticated = user.email && user.state === 'active';
+    let isAuthenticated = false;
+    if(user) {
+      isAuthenticated = user.email && user.state === 'active';
+    }
+
     return (
       <MuiThemeProvider theme={muiTheme}>
         <Switch>
